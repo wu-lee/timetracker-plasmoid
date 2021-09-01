8f267f83f588d4847a645fa22b483946863b2899
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2021-09-01 23:43:01 +0100

    ui/parseTasks.js - fix parsing bugs

--- package/contents/ui/parseTasks.js
+++ package/contents/ui/parseTasks.js
@@ -81,7 +81,8 @@ function parseTasks(eventList) {
                 break
                 
             case 'switch':
-                addTaskTime(taskEntry)
+                addTaskTime(taskEntry);
+                switchTask(taskEntry);
                 break
 
             case 'init':
@@ -108,20 +109,24 @@ function parseTasks(eventList) {
                 var startTime = new Date(taskEntry.prevTime)
                 var stopTime = new Date(taskEntry.time)
                 var milliseconds = stopTime.getTime() - startTime.getTime()
-
+                
                 if (!index[currentTask])
                     index[currentTask] = 0
-                index[currentTask] = Math.round(milliseconds/1000)
+                index[currentTask] += Math.round(milliseconds/1000)
             }
-            
+        }
+        function switchTask(taskEntry) {
             currentTask = taskEntry.param
             if (!index[currentTask])
                 index[currentTask] = 0
         }
         function initTask(taskEntry) {
             // If we were working, discard state and start afresh
-            currentTask === undefined
+            currentTask = undefined
         }
     }
 }
 
+// NodeJS accomodation
+if (module)
+    module.exports = { parseTasks, schemaVersion };
