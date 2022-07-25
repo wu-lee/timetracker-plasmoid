17ef01fa0ffd4356198f6ac74e472c70aa77e757
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2022-07-25 22:33:30 +0100

    scripts/reportJson.mjs - support filtering tasks

--- scripts/reportJson.mjs
+++ scripts/reportJson.mjs
@@ -2,9 +2,15 @@
 import { parseTasks, mkReportAccumulator } from '../package/contents/ui/parseTasks.mjs';
 import { durationHourDecimal } from '../package/contents/ui/dateFormat.mjs';
 import fs from 'fs';
+import process from 'process';
+
+var regexps;
+if (process.argv.length > 2) {
+    regexps = process.argv.slice(2);
+}
 
 const data = fs.readFileSync('/home/nick/tasks.log').toString();
-const output = parseTasks(data, mkReportAccumulator());
+const output = parseTasks(data, mkReportAccumulator(regexps));
 
 console.log(output);
 
