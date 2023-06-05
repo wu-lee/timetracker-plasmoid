db64644363e5583073a61fbf71063c10620337c3
Nick Stokoe <git.wu-lee@noodlefactory.co.uk>
2023-06-05 11:46:32 +0100

    scripts/reportTsv.mjs - untabify, reindent

--- scripts/reportTsv.mjs
+++ scripts/reportTsv.mjs
@@ -1,4 +1,4 @@
-#!/bin/env node 
+#!/bin/env node
 import { parseTasks, mkReportAccumulator } from '../package/contents/ui/parseTasks.mjs';
 import { durationHourDecimal } from '../package/contents/ui/dateFormat.mjs';
 import fs from 'fs';
@@ -14,34 +14,34 @@ function mkTsvReportAccumulator() {
     var reportAccumulator = mkReportAccumulator(regexps);
 
     function escape(str) {
-	return str
-	    .replace(/;/g, '\\;')
-	    .replace(/\t/g, '\\t');
+        return str
+            .replace(/;/g, '\\;')
+            .replace(/\t/g, '\\t');
     }
 
     return {
-	add: reportAccumulator.add,
-	result: () => {
-	    var index = reportAccumulator.index();
-	    
-	    var rows = Object.keys(index)
-		.sort()
-		.map(date => {
-		    var tasks = index[date];
-		    var total = Object.values(tasks)
-			.reduce((t, n) => t+n, 0);
-		    var names = Object.keys(tasks)
-			.sort()
-			.map(name => `${name} (${durationHourDecimal(tasks[name])})`)
-			.map(escape)
-			.join('; ');
-		    
-		    return [date,
-			    Math.round(total/(60*30))/2,
-			    names].join('\t');
-		});
-	    return rows.join('\n');
-	}
+        add: reportAccumulator.add,
+        result: () => {
+            var index = reportAccumulator.index();
+
+            var rows = Object.keys(index)
+                             .sort()
+                             .map(date => {
+                                 var tasks = index[date];
+                                 var total = Object.values(tasks)
+                                                   .reduce((t, n) => t+n, 0);
+                                 var names = Object.keys(tasks)
+                                                   .sort()
+                                                   .map(name => `${name} (${durationHourDecimal(tasks[name])})`)
+                                                   .map(escape)
+                                                   .join('; ');
+
+                                 return [date,
+                                         Math.round(total/(60*30))/2,
+                                         names].join('\t');
+                             });
+            return rows.join('\n');
+        }
     };
 }
 
@@ -50,4 +50,3 @@ const data = fs.readFileSync('/home/nick/tasks.log').toString();
 const output = parseTasks(data, mkTsvReportAccumulator());
 
 console.log(output);
-
